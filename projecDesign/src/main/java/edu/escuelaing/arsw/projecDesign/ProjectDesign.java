99e43952bc7c6f798d861299a42c53275582de01
Santiago Laiton <santiago.laiton@mail.escuelaing.edu.co>
2021-03-04 21:00:24 -0500

    main y bongo

---
+++ projecDesign/src/main/java/edu/escuelaing/arsw/projecDesign/ProjectDesign.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arsw.projecDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class ProjectDesign
+{
+    public static void main( String[] args ) {
+        //SpringApplication.run(RouletteApplication.class, args);
+        //mongodb+srv://andresDavila:1234@projectdesign.b0o7s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+        SpringApplication app = new SpringApplication(ProjectDesign.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.data.mongodb.uri", "mongodb+srv://andresDavila:1234@projectdesign.b0o7s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"));
+        app.run(args);
+    }
+}
